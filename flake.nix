7c175ef9ad4b32f18c1e51a81d274c5c8eff01f9
CollCaz <yassentombok@proton.me>
2024-11-24 01:05:42 +0200

    fixed hardware config

--- flake.nix
+++ flake.nix
@@ -15,6 +15,19 @@
       system = "x86_64-linux";
     in {
       nixosConfigurations = {
+        orthus = nixpkgs.lib.nixosSystem {
+          inherit system;
+          specialArgs = { inherit inputs; };
+          modules = [
+            ./hosts/orthus/configuration.nix
+            inputs.stylix.nixosModules.stylix
+            home-manager.nixosModules.home-manager {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.coll = import ./home.nix;
+            }
+          ];
+        };
         beatrix = nixpkgs.lib.nixosSystem {
           inherit system;
           specialArgs = { inherit inputs; };
--- hosts/orthus/configuration.nix
+++ hosts/orthus/configuration.nix
@@ -3,6 +3,7 @@
 {
 	imports = 
 		[
+			./hardware-configuration.nix
 			../core.nix
 			../nixosModules/pc/default.nix
 			../nixosModules/networkModule.nix
