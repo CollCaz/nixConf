c0d813b107a4f542c8a534332f4578102e980e47
CollCaz <62307305+CollCaz@users.noreply.github.com>
2024-11-23 06:51:26 +0200

    stylix fonts

--- hosts/nixosModules/pc/stylix.nix
+++ hosts/nixosModules/pc/stylix.nix
@@ -4,6 +4,17 @@
   stylix = {
     image = ./gruvbox_castle.png;
     enable = true;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
+
+    fonts = {
+      sansSerif = {
+        package = pkgs.fira-go;
+        name = "FiraGo";
+      };
+      monoSpace = {
+        package = pkgs.fira-code-nerdfont;
+        name = "Fira Mono Code NerdFont";
+      };
+    };
   };
 }
