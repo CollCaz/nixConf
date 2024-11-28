ebf43bd08c3f7052e56162c7a89740345b336e55
CollCaz <yassentombok@proton.me>
2024-11-28 12:49:26 +0200

    nvidia support
    typo
    
    typo

--- hosts/orthus/configuration.nix
+++ hosts/orthus/configuration.nix
@@ -8,10 +8,13 @@
 			../nixosModules/pc/default.nix
 			../nixosModules/networking/default.nix
 			../nixosModules/gaming/default.nix
+			../nixosModules/nvidia/default.nix
 
 			./devices.nix
 		];
 
 	networking.hostName = "orthus";
 	time.timeZone = "Africa/Tripoli";
+
+	nvidiaModule.enable = true;
 }
