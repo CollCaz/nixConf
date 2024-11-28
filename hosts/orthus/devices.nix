ba815f2bef39bb7634af394ccc72cd3985b05ef3
CollCaz <yassentombok@proton.me>
2024-11-28 12:30:20 +0200

    fixed devies.nix
    a

--- hosts/orthus/devices.nix
+++ hosts/orthus/devices.nix
@@ -1,9 +1,13 @@
-{ ... }:
+{ pkgs, ... }:
 {
+	environment.systemPackages = with pkgs; [
+		ntfs3g
+	];
 	fileSystems = {
 		"/mnt/Drive1" = {
 			device = "/dev/sdb2";
 			fsType = "ntfs-3g";
+			options = [ "rw" "uid=1000" ];
 		};
 	};
 }
