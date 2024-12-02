1a94a737aa9e6773f32cb27af8210edf94a8bbcc
CollCaz <yassentombok@proton.me>
2024-12-02 14:51:50 +0200

    mount by uuid

--- hosts/orthus/devices.nix
+++ hosts/orthus/devices.nix
@@ -5,7 +5,7 @@
 	];
 	fileSystems = {
 		"/mnt/Drive1" = {
-			device = "/dev/sdb2";
+			device = "/dev/disk/by-uuid/12CC5B63CC5B3FE1";
 			fsType = "ntfs-3g";
 			options = [ "rw" "uid=1000" ];
 		};
