7343e9699d6d16385db1cc4e70b6afcf6e25a50a
CollCaz <yassentombok@proton.me>
2024-12-18 08:14:08 +0200

    vbox

--- hosts/nixosModules/virtualization/default.nix
+++ hosts/nixosModules/virtualization/default.nix
@@ -1,8 +1,26 @@
-{ pkgs, ... }:
+{ lib, config, ... }:
 {
-	virtualisation.podman = {
-		enable = true;
-		dockerCompat = true;
+	options = {
+		virtualisationModule = {
+			enable =
+				lib.mkEnableOption "enable virtualisation module";
+			vbox = {
+				enable = lib.mkEnableOption "enable virtual box";
+				enableKvm = lib.mkEnableOption "enable kvm support";
+			};
+		};
 	};
-	environment.systemPackages = [ pkgs.distrobox ];
+	config = lib.mkIf config.virtualisationModule.enable {
+		# virtualisation.podman = {
+		# 	enable = true;
+		# 	dockerCompat = true;
+		# };
+		# environment.systemPackages = [ pkgs.distrobox ];
+		virtualisation.virtualbox = lib.mkIf config.virtualisationModule.enableVirtualBox {
+			host = {
+				enable = true;
+				enableKvm = true;
+			};
+		};
+	};
 }
