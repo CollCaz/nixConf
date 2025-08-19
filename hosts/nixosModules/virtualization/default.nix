66d471e4577af6a148d8c00ab4121a6ad09c832c
CollCaz <yassentombok@proton.me>
2025-08-19 19:33:08 +0200

    added virtmanager to virtualization module

--- hosts/nixosModules/virtualization/default.nix
+++ hosts/nixosModules/virtualization/default.nix
@@ -7,21 +7,30 @@
 			vbox = {
 				enable = lib.mkEnableOption "enable virtual box";
 			};
+			virtManager = {
+				enable = lib.mkEnableOption "enable virt manager";
+			};
 		};
 	};
 	config = lib.mkIf config.virtualisationModule.enable {
-		# virtualisation.podman = {
-		# 	enable = true;
-		# 	dockerCompat = true;
-		# };
-		# environment.systemPackages = [ pkgs.distrobox ];
 		users.extraGroups.vboxusers.members = [ "coll" ];
-		
+		users.extraGroups.libvirtd.members = [ "coll" ];
+
+		programs.virt-manager.enable = true;
+
 		virtualisation = {
+			spiceUSBRedirection = {
+				enable = true;
+			};
+
 			podman = {
 				enable = true;
 				dockerCompat = true;
 				defaultNetwork.settings = { dns_enabled = true; };
+			};
+
+			libvirtd = lib.mkIf config.virtualisationModule.virtManager.enable {
+				enable = true;
 			};
 
 			virtualbox = lib.mkIf config.virtualisationModule.vbox.enable {
