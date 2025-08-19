cd4857f4965a0cd542789ff5402771050f424b74
CollCaz <yassentombok@proton.me>
2025-08-19 19:32:14 +0200

    aliased cat to bat

--- homeManagerModules/shell/shellModule.nix
+++ homeManagerModules/shell/shellModule.nix
@@ -50,6 +50,7 @@
       preferAbbrs = true;
 
       shellAliases = {
+        cat = "bat";
         ls = "eza --icons always --color auto";
         fzf = "fzf  --color fg:#ebdbb2,bg:#282828,hl:#fabd2f,fg+:#ebdbb2,bg+:#3c3836,hl+:#fabd2f --color info:#83a598,prompt:#bdae93,spinner:#fabd2f,pointer:#83a598,marker:#fe8019,header:#665c54";
       };
