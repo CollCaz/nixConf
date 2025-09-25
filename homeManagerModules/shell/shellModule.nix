db22848b1da115180c28a63b2d1463affe5fc239
CollCaz <yassentombok@proton.me>
2025-09-25 23:37:14 +0200

    tldr + tmux

--- homeManagerModules/shell/shellModule.nix
+++ homeManagerModules/shell/shellModule.nix
@@ -26,6 +26,8 @@
       cowsay
       lazygit
       gh
+      tldr
+      tmux
     ];
 
     programs.fzf = {
@@ -51,6 +53,7 @@
       preferAbbrs = true;
 
       shellAliases = {
+        lg = "lazygit";
         cat = "bat";
         ls = "eza --icons always --color auto";
         fzf = "fzf  --color fg:#ebdbb2,bg:#282828,hl:#fabd2f,fg+:#ebdbb2,bg+:#3c3836,hl+:#fabd2f --color info:#83a598,prompt:#bdae93,spinner:#fabd2f,pointer:#83a598,marker:#fe8019,header:#665c54";
