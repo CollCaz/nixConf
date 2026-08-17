3af6e20bad5baa5ce3e248eb8a742ff88ab76562
CollCaz <62307305+CollCaz@users.noreply.github.com>
2026-08-17 10:28:02 +0200

    postgres

--- hosts/nixosModules/database/postgres.nix
+++ hosts/nixosModules/database/postgres.nix
@@ -9,8 +9,14 @@
 			enable = true;
 			ensureDatabases = [ "mydatabase" ];
 			authentication = pkgs.lib.mkOverride 10 ''
-				#type database DBuser auth-method
-				local all      all    trust
+				#type database DBuser origin-address auth-method
+				local all      all     trust
+				# ... other auth rules ...
+
+				# ipv4
+				host  all      all     127.0.0.1/32   trust
+				# ipv6
+				host  all      all     ::1/128        trust
 			'';
 		};
 	};
