d1ae8c9e1b86319fbee2296cbdf8878f4bba5f4a
CollCaz <yassentombok@proton.me>
2026-08-20 09:13:25 +0200

    copied old config manually because it got messed up during merge ig

--- config/hypr/fileSearch.sh
+++ config/hypr/fileSearch.sh
@@ -2,7 +2,7 @@
 
 exclude='/go'
 
-rofiCommand="rofi -async-pre-read 1 -dmenu"
+tofiCommand="tofi --font /home/coll/.nix-profile/share/fonts/opentype/FiraSans-Regular.otf --num-results=8 --width=90% --height=45% --num-results=15 --fuzzy-match=true --font-size=12"
 
 function find () {
   fd -H -a --exclude $exclude --base-directory $1
@@ -10,9 +10,10 @@ function find () {
 
 toSearch=$(find ~/ && find ~/.local/share/bottles/bottles/)
 
-pkill tofi || OUTPUT=$(echo "$toSearch" | $rofiCommand)
+pkill tofi || OUTPUT=$(echo "$toSearch" | $tofiCommand)
 echo "$OUTPUT"
 
-if [ "$OUTPUT" ]; then
+if [ "$OUTPUT" ]
+then
   kitty -e yazi "$OUTPUT"
 fi
